a46fe4ca58482353248f1920fcc7f6d3fca6b1a0
Royendgel Silberie <royendgel@gmail.com>
2015-02-21 02:38:55 -0400

    fix pypi

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name = "djangocms-revolutionslider",
+    version = "0.1",
+    author = "Royendgel Silberie",
+    author_email = "royendgel@techprocur.com",
+    description = "Quick and easy Implementation of revolution slider",
+    keywords='revolution slider, django, django-cms',
+    url = "https://github.com/royendgel",
+    packages=['migrations'],
+    classifiers=[
+      'Operating System :: MacOS :: MacOS X',
+      'Operating System :: Unix',
+      'Development Status :: 3 - Alpha',
+      'Topic :: Utilities',
+      'Programming Language :: Python :: 2.7',
+    ],
+    include_package_data=True,
+)
